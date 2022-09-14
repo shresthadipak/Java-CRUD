b0dbc2472efd639bbba4836d03414ef115803f82
Dipak <shresthadipak2049@gmail.com>
2022-09-14 09:52:52 +0545

    User: add unique constraint for email

--- src/main/java/com/arnav/StudentInfo/domain/Users.java
+++ src/main/java/com/arnav/StudentInfo/domain/Users.java
@@ -5,21 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@EnableJpaRepositories
 @Entity
-@Table(name="users")
+@Table(name="users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class Users {
 	
 	@Id
@@ -29,5 +18,35 @@ public class Users {
 	private String last_name;
 	private String email;
 	private String password;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
 }
